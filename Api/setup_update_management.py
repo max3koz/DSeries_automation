048f16de0e70d7a4bb23c2efe51198a564a7693a
Maksym Trykoz <max3koz@gmail.com>
2025-02-03 23:36:11 +0100

    DSER-018 Updated setup_update_management functions

--- Api/setup_update_management.py
+++ Api/setup_update_management.py
@@ -5,18 +5,24 @@ def update_config_dict(config_dict, update_data):
         else:
             config_dict[section] = values
 
-def create_file_from_dict(connect_session, file_path, config_dict):
-    with open(file_path, 'w') as f:
-        for section, values in config_dict.items():
-            f.write(f'[{section}]\n')
-            for key, value in values.items():
-                f.write(f'{key}={value}\n')
-    with open(file_path, 'r') as f:
-        content = f.read()
-    commands = [(f'cmd /c "cd C:\Program Files\D-Services && echo {line.strip()} >> '
-                 f'{file_path}"') for line in content.split('\n') if line.strip()]
-    for command in commands:
-        connect_session.run_cmd(command)
+def dict_to_string(config_dict):
+    result = []
+    for section, values in config_dict.items():
+        result.append(f'[{section}]')
+        for key, value in values.items():
+            result.append(f'{key}={value}')
+    return '\r\n'.join(result)
+
+def build_command_string(file_path, content):
+    command_lines = content.split('\n')
+    commands = ' && '.join([f'echo {line} >> {file_path}' for line in command_lines if line])
+    command = f'cmd /c "cd C:\\Program Files\\D-Services && {commands}"'
+    return command
+
+def create_file_on_dservices_server(connect_session, file_path, config_dict):
+    content = dict_to_string(config_dict)
+    command = build_command_string(file_path, content)
+    connect_session.run_cmd(command)
 
 def copy_file_on_server(connect_session, sample_file, result_file):
     # 'copy system_default.cfg system.cfg'
@@ -35,6 +41,5 @@ def get_cmd_output(connect_session, command):
     return result.std_out.decode("utf-8")
 
 def verify_service_run(server_connect_session, process_name):
-    process = get_cmd_output(server_connect_session,
-                                    f"tasklist /FI \"IMAGENAME eq {process_name}.exe\"")
+    process = get_cmd_output(server_connect_session,f"tasklist /FI \"IMAGENAME eq {process_name}.exe\"")
     return process_name in process
